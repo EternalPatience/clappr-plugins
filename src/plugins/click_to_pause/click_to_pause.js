b83a11c58a4a4cb47ba1ad649bae235224450592
Ramazan Zhakupov <zhakupov.rm2112@gmail.com>
2023-03-14 11:29:36 +0300

    Refactor ClickToPause to support touch events
    * This feature provides better UX for mobile devices
    * Extends UICorePlugin to use render method and get access to media control
    * Add play/pause icons that duplicate media control button state
    * Rewrite settingsUpdate for better reading

--- src/plugins/click_to_pause/click_to_pause.js
+++ src/plugins/click_to_pause/click_to_pause.js
@@ -2,40 +2,107 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import { ContainerPlugin, Events, Playback } from '@clappr/core'
+import { Events, Playback, Styler, UICorePlugin } from '@clappr/core'
 
-export default class ClickToPausePlugin extends ContainerPlugin {
+import clickToPauseHTML from './public/click-to-pause.html'
+import clickToPauseStyles from './public/click-to-pause.scss'
+
+import playIcon from '../../icons/01-play.svg'
+import pauseIcon from '../../icons/02-pause.svg'
+
+export default class ClickToPausePlugin extends UICorePlugin {
   get name() { return 'click_to_pause' }
   get supportedVersion() { return { min: CLAPPR_CORE_VERSION } }
   get config() { return this.container.options.clickToPauseConfig || {} }
-
-  constructor(container) {
-    super(container)
-  }
-
-  bindEvents() {
-    this.listenTo(this.container, Events.CONTAINER_CLICK, this.click)
-    this.listenTo(this.container, Events.CONTAINER_SETTINGSUPDATE, this.settingsUpdate)
-  }
-
-  click() {
-    const onClickPayload = this.config.onClickPayload
-
-    if (this.container.getPlaybackType() !== Playback.LIVE || this.container.isDvrEnabled()) {
-      if (this.container.isPlaying())
-        this.container.pause(onClickPayload)
-      else
-        this.container.play(onClickPayload)
-
+  get container() { return this.core && this.core.activeContainer }
+  get template() { return clickToPauseHTML }
+  get attributes() {
+    return {
+      class: 'player-click-to-pause'
     }
   }
 
+  bindEvents() {
+    this.listenTo(this.core, Events.CORE_ACTIVE_CONTAINER_CHANGED, this.onContainerChange)
+  }
+
+  onContainerChange() {
+    this.render()
+    this.listenTo(this.container, Events.CONTAINER_SETTINGSUPDATE, this.settingsUpdate)
+    this.listenTo(this.container, Events.CONTAINER_CLICK, this.onClick)
+    this.listenTo(this.container, Events.CONTAINER_TAP, this.onTouch)
+    this.listenTo(this.container, Events.CONTAINER_MEDIACONTROL_HIDE, this.hideIcon)
+    this.listenTo(this.container, Events.CONTAINER_MEDIACONTROL_SHOW, this.showIcon)
+    this.listenTo(this.container, Events.CONTAINER_PAUSE, this.showIcon)
+    this.listenTo(this.container, Events.CONTAINER_PLAY, this.showIcon)
+  }
+
+  showIcon() {
+    if (!this.pausable()) return
+    if (!this.core.mediaControl.isVisible()) return
+
+    if (this.container.isPlaying()) {
+      this.$iconWrapper.data('pause', '')
+      this.$iconWrapper.data('play', null)
+    } else {
+      this.$iconWrapper.data('play', '')
+      this.$iconWrapper.data('pause', null)
+    }
+  }
+
+  hideIcon() {
+    if (!this.container.isPlaying()) return
+    this.$iconWrapper.data('pause', null)
+    this.$iconWrapper.data('play', null)
+  }
+
+  onClick() {
+    if (!this.pausable()) return
+    this.core.mediaControl.show()
+    const onClickPayload = this.config.onClickPayload
+
+    if (this.container.isPlaying())
+      this.container.pause(onClickPayload)
+    else
+      this.container.play(onClickPayload)
+
+  }
+
+  onTouch() {
+    if (this.core.mediaControl.isVisible())
+      this.container.trigger(Events.CONTAINER_CLICK)
+    else
+      this.core.mediaControl.show()
+
+  }
+
+  pausable() {
+    return (this.container.getPlaybackType() !== Playback.LIVE || this.container.isDvrEnabled())
+  }
+
   settingsUpdate() {
-    const pointerEnabled = this.container.getPlaybackType() !== Playback.LIVE || this.container.isDvrEnabled()
+    const pointerEnabled = this.pausable()
+
     if (pointerEnabled === this.pointerEnabled) return
 
-    const method = pointerEnabled ? 'addClass' : 'removeClass'
-    this.container.$el[method]('pointer-enabled')
+    if (pointerEnabled) this.container.$el.addClass('pointer-enabled')
+    else this.container.$el.removeClass('pointer-enabled')
+
     this.pointerEnabled = pointerEnabled
+  }
+
+  render() {
+    if (!this.container) return
+    this.$el.html(this.template)
+    const style = Styler.getStyleFor(clickToPauseStyles)
+
+    this.$iconWrapper = this.$el.find('.icon-wrapper')
+    this.$playButton = this.$el.find('.play-button')
+    this.$playButton.append(playIcon)
+    this.$pauseButton = this.$el.find('.pause-button')
+    this.$pauseButton.append(pauseIcon)
+
+    this.$el.append(style)
+    this.container.$el.append(this.$el)
   }
 }
